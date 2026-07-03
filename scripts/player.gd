bdf04e8b8548abbfc737bc58cfbd5db138e3e9b3
Anthony Stenmark <stenners@2407-e400-8009-d200-1090-5f9f-36d-3461-cpe.spintel.net.au>
2026-07-03 18:52:10 +1000

    initial commit

---
+++ scripts/player.gd
@@ -0,0 +1,66 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+var last_direction:Vector2 = Vector2.RIGHT
+var is_attacking: bool = false
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+# ===============================================
+#  MOVEMENT & ANIMATION
+# ===============================================
+
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_just_pressed("attack_1") and not is_attacking:
+		attacks("attack_1")
+		
+	# No movement if attacking
+	if is_attacking:
+		velocity = Vector2.ZERO
+		return
+
+	process_movement()
+	process_animation()
+	move_and_slide()
+
+func process_movement() -> void:
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_vector("left", "right", "up", "down")
+	if direction != Vector2.ZERO:
+		velocity = direction * SPEED
+		last_direction = direction
+	else:
+		velocity = Vector2.ZERO
+
+func process_animation() -> void:
+	if is_attacking:
+		return
+	if velocity != Vector2.ZERO:
+		play_animation("run", last_direction)
+	else:
+		play_animation("idle", last_direction)
+
+func play_animation(prefix: String, dir: Vector2) -> void:
+	if dir.x != 0:
+		animated_sprite_2d.flip_h = dir.x < 0
+		animated_sprite_2d.play(prefix + "_side")
+	elif dir.y < 0:
+		animated_sprite_2d.play(prefix + "_up")
+	elif dir.y > 0:
+		animated_sprite_2d.play(prefix + "_down")
+
+# ===============================================
+#  ATTACKS
+# ===============================================
+func attacks(type: String) -> void: 
+	is_attacking = true
+	play_animation(type, last_direction)
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	is_attacking = false
+
+
+# ===============================================
+#  HITBOX
+# ===============================================
